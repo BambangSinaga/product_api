afc9ffe6fec990c38e3d057409b988fff10d5735
BambangSinaga <mejbambang@gmail.com>
2020-11-22 20:51:46 +0700

    add email validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,6 @@
 class Product < ApplicationRecord
   validates :name, :price, :category, :email, presence: true
+
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP, message: "Email invalid"  },
+            uniqueness: { case_sensitive: false }
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -5,4 +5,24 @@ RSpec.describe Product, type: :model do
     is_expected.to validate_presence_of(:category)
     is_expected.to validate_presence_of(:email)
   }
+
+  describe '.email' do
+    subject { FactoryBot.build(:product, email: email) }
+
+    context 'when valid' do
+      let(:email) { 'sample@mail.com' }
+
+      it 'returns valid' do
+        expect(subject).to be_valid
+      end
+    end
+
+    context 'when invalid' do
+      let(:email) { 'samplemail.com' }
+
+      it 'returns invalid' do
+        expect(subject).not_to be_valid
+      end
+    end
+  end
 end
