198222b3e1b82ce8f58e202788d7190d5c2d5be5
BambangSinaga <mejbambang@gmail.com>
2020-11-22 23:10:38 +0700

    remove method image_url from model

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,9 +5,4 @@ class Product < ApplicationRecord
 
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP, message: "Email invalid"  },
             uniqueness: { case_sensitive: false }
-
-  def image_url
-    return Rails.application.routes.url_helpers.url_for(image) if image.attached?
-    ''
-  end
 end
